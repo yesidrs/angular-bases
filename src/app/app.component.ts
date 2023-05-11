a79bdbb9332179f9611de11585713b7cb819d41b
yesiddev <yesiddeveloper@gmail.com>
2023-05-11 06:32:20 -0500

    clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Counter';
+  public number: number = 0;
+  @Input() increase: number = 0; 
+
+  public increment(): void {
+    this.number++;
+  }
+
+  public decrement(): void {
+    if(this.number > 0) {
+      this.number--;
+    } else {
+      alert('This is the minimum value!');
+    }
+  }
+
+  public reset(): void {
+    this.number = 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
