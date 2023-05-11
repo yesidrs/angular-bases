e385fa3a69dc165f84245a16acbb714bf9fc8810
yesiddev <yesiddeveloper@gmail.com>
2023-05-11 17:34:46 -0500

    add eventi binding and ngif directive

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent {
+  superhero = 'Spiderman';
+  name = 'Peter Parker';
+  age = 16;
 
+  get capitalizeName(): string {
+    return this.superhero.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} is ${this.age} years old`;
+  }
+
+  changeHero(): void {
+    this.superhero = 'Ironman';
+    this.name = 'Tony Stark';
+    this.age = 45;
+  }
+
+  reset(): void {
+    this.superhero = 'Spiderman';
+    this.name = 'Peter Parker';
+    this.age = 16;
+  }
 }
