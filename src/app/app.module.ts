4aeaeffa56696cd41f42e28d5e6ab7345c83260e
yesiddev <yesiddeveloper@gmail.com>
2023-05-11 16:07:00 -0500

    add new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule],
+  imports: [BrowserModule, CounterModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
