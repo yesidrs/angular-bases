c67eb7ed06022777172f8c0beaf42ec982da2ce6
yesiddev <yesiddeveloper@gmail.com>
2023-05-15 16:58:35 -0500

    add new module

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-page.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { ListComponent } from './components/list/list.component';
+
+@NgModule({
+  declarations: [MainPageComponent, AddCharacterComponent, ListComponent],
+  exports: [MainPageComponent],
+  imports: [CommonModule],
+})
+export class DbzModule {}
